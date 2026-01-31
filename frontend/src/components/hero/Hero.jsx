2b8f70fc53738ea7aed1c7d47d7ede965d5c6939
Rusiru Jayanga <rusirujayanga@gmail.com>
2026-01-31 13:43:14 +0530

    Front-end 6
    Hero section is continuing.

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 function Hero() {
 
   return (
-    <section className='mt-5 p-2 grid gap-4'>
+    <section className='p-2 grid gap-4'>
+        <div className='w-full h-50 flex justify-end items-center overflow-hidden bg-[linear-gradient(0deg,rgba(12,12,12,0.4)_0%,rgba(12,12,12,0.3)_100%),url(/hero-main-sm.jfif)] bg-cover bg-center p-2 rounded-3xl'>
+            <img className='w-40' src="titleLogo.png" alt="logo" />
+        </div>
         <div className="w-full bg-white p-5 rounded-3xl">
-            <h1 className='text-7xl font-bold'>Hello!</h1>
+            <h1 className='text-7xl font-bold'>Hello<span>!</span></h1>
             <div className='mt-5 ml-5 text-sm font-medium'>
             <p><span className='text-blue-600'>function</span> <span className='text-yellow-400'>App</span> <span className='text-red-600'>&#40;&#41;</span> <span className='text-red-600'>&#123;</span></p>
             <p className='ml-5'><span className='text-purple-500'>return</span> <span className='text-green-500'>&#40;</span></p>
@@ -20,16 +23,7 @@ function Hero() {
             </div>
             <div className='flex gap-2 mt-10'><button className='flex w-37 border-2 border-[#ecf95a] rounded-md bg-[#ecf95a] text-[#191314] hover:bg-white hover:text-[#191314] focus:bg-white focus:text-[#191314] '>Download CV</button><button className='flex w-37 border-2 border-[#191314] rounded-md bg-white text-[#191314] hover:bg-[#ecf95a] hover:text-[#191314] hover:border-[#ecf95a] focus:bg-[#ecf95a] focus:text-[#191314] focus:border-[#ecf95a] '>Contact</button></div>
         </div>
-        <div className='relative'>
-            <img className='w-full h-3/4 object-cover rounded-3xl' src="hero-main.jfif" alt="" />
-            <div className="absolute top-5 left-5 bg-white/50 p-3 rounded-xl h-fit flex gap-2">
-                <div className="size-10 bg-amber-400 rounded-full"></div>
-                <div className="size-10 bg-amber-400 rounded-full"></div>
-                <div className="size-10 bg-amber-400 rounded-full"></div>
-                <div className="size-10 bg-amber-400 rounded-full"></div>
-                <div className="size-10 bg-amber-400 rounded-full"></div>
-            </div>
-        </div>
+        
     </section>
   )
 }
