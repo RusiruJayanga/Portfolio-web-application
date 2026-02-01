0312379b4ecae9f47632ce14b1e793a419c03036
Rusiru Jayanga <rusirujayanga@gmail.com>
2026-02-01 23:04:40 +0530

    Front-end 7
    Navigation bar lg animation is completed.

--- frontend/src/components/navigation/NavigationBar.jsx
+++ frontend/src/components/navigation/NavigationBar.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 //GSAP animation
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const NavigationBar = () => {
   //menu toggle
@@ -11,6 +13,22 @@ const NavigationBar = () => {
 
   //menu GSAP animation
   const navRef = useRef(null);
+
+  useLayoutEffect(() => {
+    if (!navRef.current) return;
+    
+    gsap.to(navRef.current, {
+    top: "8px",
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    scrollTrigger: {
+      trigger: navRef.current,
+      start: "bottom 60px",
+      end: "bottom 0px",
+      scrub: true,
+    },
+  });
+  }, []);
+
   useEffect(() => {
     if (!navRef.current) return;
      if (isOpen === true) {
@@ -45,17 +63,17 @@ const NavigationBar = () => {
       gsap.to("#nav-close", {
         duration: 0.3,
         rotate: 0,
-        ease: "power3.out",
+        ease: "power3.in",
       });
     }
   }, [isOpen]);
     
 
   return (
-    <nav className="relative w-full h-15 flex items-center z-10000 text-base font-normal" >
+    <nav className="relative w-screen h-15 flex items-center z-10000 text-base font-normal lg:w-fit lg:sticky lg:ml-auto lg:mr-auto lg:rounded-[30px]" ref={navRef}>
 
       {/*lg menu */}
-      <div className="hidden lg:flex w-fit ml-auto mr-auto gap-5 text-black/60">
+      <div className="hidden lg:flex w-fit ml-auto mr-auto gap-5 text-black/60 lg:pr-10 lg:pl-10">
         <a className={`${isActive === "home" ? "text-black font-bold" : ""} hover:text-black transition-colors duration-200 ease-out`} href="#" onClick={()=> setIsActive("home")}>
           Home
         </a>
@@ -80,17 +98,17 @@ const NavigationBar = () => {
       ></i>
       <div
         className="hidden fixed w-screen h-screen rounded-lg top-0 left-0 bg-black/30 z-1000"
+        onClick={() => setIsOpen(!isOpen)}
         id="nav-slider-container"
-        ref={navRef}
       ></div>
-        <div className="hidden fixed w-6/10 h-screen top-0 right-0 ml-auto bg-white sm:w-1/3 z-11000"
+        <div className="hidden fixed w-3/5 h-screen top-0 right-0 ml-auto bg-white sm:w-1/3 z-11000"
         id="nav-slider">
           <i
             className="bi bi-x-lg size-10 flex items-center justify-center text-2xl p-7 rounded-full focus:bg-[#f4f4f4]"
             onClick={() => setIsOpen(!isOpen)}
             id="nav-close"
           ></i>
-          <div className="w-2/3 ml-auto mr-2 mt-30 flex flex-col text-black/60 ">
+          <div className="w-2/3 ml-auto mr-auto mt-30 flex flex-col text-black/60 ">
             <a
               className={`${isActive === "home" ? "text-black font-bold" : ""} w-full p-2 rounded-sm flex justify-between focus:bg-[#f4f4f4] `}
               href="#"
@@ -126,6 +144,12 @@ const NavigationBar = () => {
             >
               Projects<i className="bi bi-chevron-right"></i>
             </a>
+          </div>
+          <div className="w-full absolute bottom-10 flex gap-5 text-2xl text-black/60">
+            <i className="bi bi-github ml-auto"></i>
+            <i className="bi bi-linkedin"></i>
+            <i className="bi bi-instagram"></i>
+            <i className="bi bi-google mr-auto"></i>
           </div>
         </div>
     </nav>
