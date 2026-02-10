117125556726d4fbe2c4f8887857311ada22386a
Rusiru Jayanga <rusirujayanga@gmail.com>
2026-02-10 13:08:31 +0530

    Front-end 12
    About section is completed.

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -12,13 +12,13 @@ const About = () => {
   const aboutRef = useRef(null);
   const aboutProfileConRef = useRef(null);
   const aboutProfileRef = useRef(null);
+  const aboutGalleryRef = useRef(null);
   const aboutNameRef = useRef(null);
   const aboutContentRef = useRef(null);
 
   useLayoutEffect(() => {
   if (!aboutRef.current) return;
 
-  mm.add("(max-width: 1640px)", () => {
     const tl = gsap.timeline({
     scrollTrigger: {
       trigger: aboutProfileConRef.current,
@@ -59,6 +59,17 @@ const About = () => {
   );
 
   tl.fromTo(
+    aboutGalleryRef.current,
+    { xPercent: -100,
+      opacity: 0,
+     },
+    {
+      xPercent: 0,
+      opacity: 1,
+    },
+  );
+
+  tl.fromTo(
     aboutContentRef.current,
     { 
       yPercent: -100,
@@ -72,7 +83,6 @@ const About = () => {
 
   return () => tl.kill();
   });
- }, []);
 
   return (
     <section className='p-2 mx-auto text-sm font-normal mt-10 sm:max-w-150 sm:mt-20 lg:max-w-220 lg:mt-50' ref={aboutRef}>
@@ -82,16 +92,22 @@ const About = () => {
           <p className='ml-auto'>Me.</p>
         </div> 
         <div className='w-full flex flex-col items-center gap-4 mt-5'>
-          <div className="flex items-end gap-4 p-2 bg-white rounded-3xl mt-5 z-10 " ref={aboutProfileConRef}>
+          <div className="flex relative items-end gap-2 p-2 bg-white rounded-3xl mt-5 z-10 " ref={aboutProfileConRef}>
             <img className='w-50 h-50 object-cover rounded-2xl z-20 sm:w-80 sm:h-80 lg:w-120 lg:h-100' src="profile-picture.jpg" alt="profile-picture" ref={aboutProfileRef}/>
-            <h4 className=" text-lg" ref={aboutNameRef}>Hi!, I'm <br /><span className='text-2xl font-bold xl:text-4xl'>Rusiru Jayanga</span></h4>
+            <div className="w-full h-50 flex flex-col items-start justify-between sm:h-80 lg:h-100" ref={aboutNameRef}>
+              <div className="w-full flex gap-2">
+              <img className="w-1/2 h-25 object-cover rounded-xl z-10 sm:h-50 sm:rounded-2xl lg:h-70" src="profile-picture-1.jpg" alt="profile-picture-1" />
+              <img className="w-1/2 h-25 object-cover rounded-xl sm:h-50 sm:rounded-2xl lg:h-70" src="profile-picture-2.jpg" alt="profile-picture-2" ref={aboutGalleryRef}/>
+              </div>
+               <h4 className=" text-lg" >Hi!, I'm <br /><span className='text-2xl font-bold xl:text-4xl'>Rusiru Jayanga</span></h4>
+            </div>
           </div>
           
           <div className='w-full bg-white p-5 rounded-3xl z-0' ref={aboutContentRef}>
             <p className="article columns-2 sm:hidden"> - I’m a passionate Software Engineer with a strong focus on the MERN stack.
             I enjoy building scalable, efficient, and user-friendly web applications that deliver real value. <br />
             I’m always eager to collaborate, share ideas, and grow as a developer.</p>
-            <p className="hidden sm:block article columns-2">I’m a passionate Software Engineer with a strong focus on the MERN stack. I enjoy building scalable, efficient, and user-friendly web applications that deliver real value.
+            <p className="hidden sm:block article columns-2">I’m a passionate Software Engineer with a strong focus on the MERN stack. <br />I enjoy building scalable, efficient, and user-friendly web applications that deliver real value. <br />
             My journey in software development has taught me the importance of writing clean, maintainable code and continuously learning new technologies to stay ahead in a fast-evolving industry. I’m particularly interested in full-stack development — from designing intuitive front-end interfaces to building robust back-end systems.
             <br /><br />I’m always eager to collaborate, share ideas, and grow as a developer — whether through solving complex problems, exploring new tools, or contributing to meaningful projects.</p>
           </div>
